416238a417a41e82c247cc0594d7a6c459b2318f
Mohammed Osei <chrisosei305@gmail.com>
2026-09-25 12:06:09 +0000

    push push

--- api/main.py
+++ api/main.py
@@ -50,7 +50,10 @@ cors_origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=cors_origins,
+    allow_origins=[
+        "https://konkonsa-frontend-lwf8.vercel.app",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
